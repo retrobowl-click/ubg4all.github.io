0b3cb3c4174ac2565971b1d296660442adac6c51
tutpa199x <tupta1999@gmail.com>
2024-02-25 21:41:23 +0700

    house of hazards

---
+++ game/house of hazards/config.js
@@ -0,0 +1,9 @@
+
+window.config = {
+    debug: false,
+    maxRatio: 16 / 9,
+    minRatio: 9 / 16,
+    numScreenshots: 0,
+    unityWebglBuildUrl: 'Build/HouseOfHazardsPoki.json',
+    unityWebglLoaderUrl: 'UnityLoader.js'
+};
