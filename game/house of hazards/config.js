6a44ba849131fdc1439cff561896173f7b7cb079
tutpa199x <tupta1999@gmail.com>
2024-02-25 21:54:11 +0700

    remove house of hazards NOT WORK

--- game/house of hazards/config.js
+++
@@ -1,9 +0,0 @@
-
-window.config = {
-    debug: false,
-    maxRatio: 16 / 9,
-    minRatio: 9 / 16,
-    numScreenshots: 0,
-    unityWebglBuildUrl: 'Build/HouseOfHazardsPoki.json',
-    unityWebglLoaderUrl: 'UnityLoader.js'
-};
